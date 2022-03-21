c1f05f2ea5753749a52ce8b28168b6416fb0706e
Yannik <80115275+Yannik101010@users.noreply.github.com>
2022-03-21 18:31:21 +0100

    Update util.py

--- util.py
+++ util.py
@@ -1,9 +1,21 @@
+"""
+Python script containing all global variables and filepaths.
+
+Louis Kapp, Felix Hammer, Yannik Ullrich
+"""
+
 import pandas as pd
 import numpy as np
 
+# Target Shape
 TARGET_SHAPE = (300, 300)
+# Folder in which the Training Images reside
 IMG_FOLDER = "../train_images/"
+# CSV with subset of images (most common individuals)
 IMG_CSV_SUBSET = pd.read_csv("ckpts/imgs_subset.csv").dropna().reset_index(drop=True)
+# 
 IMG_CSV = pd.read_csv("ckpts/imgs.csv").dropna().reset_index(drop=True)
+#
 IMG_SHAPES = np.load("ckpts/img_shapes.npy", allow_pickle=True)
+# 
 MOST_COMMON_INDIVIDUALS = np.load("ckpts/most_common_indices.npy", allow_pickle=True)
