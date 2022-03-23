5edc6181da00c1ecd0e9393cb9a5d4fb9a4edc4b
MockaWolke <fhammer@uni-osnabrueck.de>
2022-03-23 22:49:43 +0100

    training daten kreiren
    noch nicht alles fertig aber morgen früh sollte es gehen. hab im util vielleicht zu viel gelöscht

--- util.py
+++ util.py
@@ -9,13 +9,13 @@ import numpy as np
 
 # Target Shape
 TARGET_SHAPE = (300, 300)
-# Folder in which the Training Images reside
-IMG_FOLDER = "../input/happy-whale-and-dolphin/train_images/"
-# CSV with subset of images (most common individuals)
-IMG_CSV_SUBSET = pd.read_csv("../input/happywhale-src/happywhale-main/ckpts/imgs_subset.csv").dropna().reset_index(drop=True)
-# 
-IMG_CSV = pd.read_csv("../input/happywhale-src/happywhale-main/ckpts/imgs.csv").dropna().reset_index(drop=True)
-#
-IMG_SHAPES = np.load("../input/happywhale-src/happywhale-main/ckpts/img_shapes.npy", allow_pickle=True)
-# 
-MOST_COMMON_INDIVIDUALS = np.load("../input/happywhale-src/happywhale-main/ckpts/most_common_indices.npy", allow_pickle=True)
+# Folder in which the Original Kaggle Training Images reside
+IMG_FOLDER = "../KaggleData/train_images"
+
+IMG_CSV = pd.read_csv("../KaggleData/train.csv")
+
+UPPER_LIMIT_OF_IMAGES = 10
+
+give_nice_percentage = lambda x,y: int( 100 * np.round(x/y,2))
+
+PATH_FOR_OUR_TRAINING_DATA = "../OurTrainingData"
