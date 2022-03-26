4c4ef5386a1f90bbe1cd6dc975c811691c374c6b
MockaWolke <fhammer@uni-osnabrueck.de>
2022-03-26 19:46:18 +0100

    weiter

--- DS_Generator.py
+++ DS_Generator.py
@@ -161,38 +161,51 @@ class DS_Generator():
     """
 
     def __init__(self):
-        global PATH_FOR_OUR_TRAINING_DATA, UPPER_LIMIT_OF_IMAGES, BATCH_SIZE
-        self.folder_of_data = PATH_FOR_OUR_TRAINING_DATA
-        self.limit = UPPER_LIMIT_OF_IMAGES
-        self.batch_size = BATCH_SIZE
+        pass
 
-    def generate(self,df,augment=False,individuals=False):
+    def generate(self,df,augment=False,individuals=False,batch_size=None):
         """
         args:
         
         augment - bool / Wether you want data augmentation until upper limit"""
+        
+        if batch_size==None:
+            batch_size = BATCH_SIZE # if no batch size specified, we take the one from utils.py
+        
+        if individuals:
+            task = "individuals"
+        else:
+            task = "species"
+        
+        df = smart_batches(df,batch_size,task)
 
-        image_paths = self.folder_of_data + "/" + df["image"]
+        image_paths = PATH_FOR_OUR_TRAINING_DATA + "/" + df["image"]
         
+
         image_paths = tf.convert_to_tensor(image_paths, dtype=tf.string)
-        
+
+        # choose the wright labes according to task
         if individuals:
             labels = tf.convert_to_tensor(df["label"], dtype=tf.int32)
         else:
             labels = tf.convert_to_tensor(df["species_label"], dtype=tf.int32)
+
         ds = tf.data.Dataset.from_tensor_slices((image_paths, labels))
-        ds = tf.data.Dataset.from_tensor_slices((image_paths, labels))
+        ds = ds.map(self.prepare_images_mapping, num_parallel_calls=8)
         
-        func = self.prepare_images_mapping
-        ds = ds.map(func, num_parallel_calls=8)
-        ds = ds.batch(self.batch_size)
+        augmented_ds1 =  ds.map(self.augment1)
+        ds = ds.concatenate(augmented_ds1)
+        ds = ds.batch(batch_size)
 
         return ds
 
-    def prepare_images_mapping(self,path, label):
+    def prepare_images_mapping(self, path,  label):
         x = tf.io.read_file(path)
-        x = tf.image.decode_jpeg(x,3)
+        x = tf.image.decode_jpeg(x,channels=1)
         x = tf.cast(x, dtype=tf.float32)
         x *= (2 / 255)
         x -= 1
-        return x,label
+        return x,  label
+    
+    def augment1(self, img, label):
+        return tf.image.flip_left_right(img), label
