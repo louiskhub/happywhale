0af01766845c114ff08788fdec067cee73a3b04c
MockaWolke <fhammer@uni-osnabrueck.de>
2022-03-27 16:03:53 +0200

    mdoel_eval sinnvoller

--- model_evaluation.py
+++ model_evaluation.py
@@ -2,29 +2,17 @@ import numpy as np
 from scipy.spatial.distance import cdist
 import tensorflow_datasets as tfds
 
-def mean_average_precision(model, val_ds):
-    # get the embeddings
-    embedding_results = model.predict(val_ds)
-    # get the corresponding_labels
-    corresponding_labels = np.concatenate([label for img, label in tfds.as_numpy(val_ds)])
-    assert len(embedding_results) == len(corresponding_labels)
+def mean_average_precision(model,train_ds , val_ds):
+    embeddings = model.predict(train_ds)
 
-    # get pairwise distance matrix
-    pairwise_distances = cdist(embedding_results, embedding_results)
+    corresponding_labels = np.concatenate([label for img, label in tfds.as_numpy(train_ds)])
 
-    # array for index of closest datapoint
-    closest_data_indeces = np.zeros(len(pairwise_distances), dtype=int)
+    val_embeddings = model.predict(val_ds)
+    val_labels = np.concatenate([label for img, label in tfds.as_numpy(val_ds)])
 
-    # for every datapoint
-    for i in range(len(pairwise_distances)):
-        # get distance column
-        arr = pairwise_distances[i]
-        # remove distance to itself
-        arr = np.concatenate((arr[:i], arr[i + 1:]))
-        # get closest index
-        closest_data_indeces[i] = np.argmin(arr)
+    pairwise_distances = cdist(embeddings, val_embeddings)
 
-    # get predicted labels
-    predicted_labels = np.array([corresponding_labels[i] for i in closest_data_indeces])
+    closest_embeddings_indices = np.argmin(pairwise_distances, 0)
+    predicted_labels = np.array([corresponding_labels[i] for i in closest_embeddings_indices])
 
-    return np.mean(predicted_labels == corresponding_labels)
+    return np.mean(predicted_labels == val_labels)
