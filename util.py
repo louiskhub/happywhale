1adfa589585b6fc9121dfd3b5658d5796e8c5d1f
Louis Kapp <louis.kapp@tngtech.com>
2022-03-27 18:20:50 +0200

    cleaned up and commented visualization-notebook

--- util.py
+++ util.py
@@ -12,8 +12,8 @@ import pandas as pd
 ################################################################
 
 # original data from https://www.kaggle.com/competitions/whale-categorization-playground/data
-IMG_FOLDER = "../KaggleData/train_images"
-IMG_CSV = pd.read_csv("../KaggleData/train.csv")
+TRAIN_IMG_FOLDER = "../KaggleData/train_images"
+TRAIN_CSV = "../KaggleData/train.csv"
 
 # local path to our modified version of the kaggle data
 TRAIN_DATA_PATH = "../OurSpeciesTrainingData"
@@ -25,6 +25,8 @@ TRAIN_DATA_PATH = "../OurSpeciesTrainingData"
 
 TARGET_SHAPE = (256, 256)
 UPPER_LIMIT_OF_IMAGES = 10  # max. 10 images per individual to reduce overfitting
+
+PRELIM_TRAIN_DF = pd.read_csv(TRAIN_CSV)
 TRAIN_DF = pd.read_csv(TRAIN_DATA_PATH + "/data.csv", index_col=0)
 
 
