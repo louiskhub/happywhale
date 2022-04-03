e069c6e366cc017509369480d7ee5ad96cd367ec
MockaWolke <fhammer@uni-osnabrueck.de>
2022-04-03 14:13:15 +0200

    plots backs up being crispy

--- src/triplet_eval.py
+++ src/triplet_eval.py
@@ -7,6 +7,7 @@ import pandas as pd
 import time
 from scipy.spatial.distance import cdist
 import umap
+from matplotlib.patches import Patch
 
 
 def compute_closest_k_neighbors(a, b, k=3):
@@ -48,7 +49,7 @@ def calculate_class_accuracy(species, val_df):
     return np.array([acc, spec_acc, k5_acc])
 
 
-def create_triplet_eval(model, train_ds, val_ds, train_df, val_df, new_whales_ds, new_whales_df, path_to_save,name):
+def create_triplet_eval(model, train_ds, val_ds, train_df, val_df, new_whales_ds, new_whales_df, path_to_save, name):
     # calculate embeddings
 
     print("Calculate Embeddings")
@@ -84,13 +85,13 @@ def create_triplet_eval(model, train_ds, val_ds, train_df, val_df, new_whales_ds
     val_df["correctly_labeled_k5"] = k_accuracy(train_df, val_df, 5, val_closest_k_indices, indivums=True,
                                                 return_mean=False)
 
-    round = lambda x: np.round(x, 2)
 
-    over_all_acc = round(np.mean(val_df["correctly_labeled"]))
-    over_all_acc_species = round(np.mean(val_df["species_correctly_labeled"]))
-    over_all_top5_acc = round(np.mean(val_df["correctly_labeled_k5"]))
 
-    fig_info = name + f"Accuracies:  Individual-MAP: {over_all_acc}, Species-MAP: {over_all_acc_species}, k5-MAP - {over_all_top5_acc}"
+    over_all_acc = np.round(np.mean(val_df["correctly_labeled"]), 4)
+    over_all_acc_species = np.round(np.mean(val_df["species_correctly_labeled"]), 4)
+    over_all_top5_acc = np.round(np.mean(val_df["correctly_labeled_k5"]), 4)
+
+    fig_info = name + f"Accuracies:  Individual-MAP: {over_all_acc*100}%, Species-MAP: {over_all_acc_species*100}%, k5-MAP - {over_all_top5_acc*100}%"
 
     fig, ax = plt.subplots(figsize=(14, 14))
 
@@ -100,8 +101,7 @@ def create_triplet_eval(model, train_ds, val_ds, train_df, val_df, new_whales_ds
 
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), title="Species")
     plt.figtext(0.5, 0.08, fig_info, wrap=True, horizontalalignment='center', fontsize=14)
-    plt.title("2d UMAP Visualisation of Train Embeddings")
-
+    plt.title("2d UMAP Visualisation of Train Embeddings", fontsize=18)
 
     plt.savefig(path_to_save + "2dUMAP_train.png", bbox_inches='tight')
 
@@ -113,17 +113,17 @@ def create_triplet_eval(model, train_ds, val_ds, train_df, val_df, new_whales_ds
 
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), title="Species")
     plt.figtext(0.5, 0.08, fig_info, wrap=True, horizontalalignment='center', fontsize=14)
-    plt.title("2d UMAP Visualisation of Val Embeddings")
+    plt.title("2d UMAP Visualisation of Val Embeddings", fontsize=18)
     plt.savefig(path_to_save + "2dUMAP_val.png", bbox_inches='tight')
 
     # create daframe for density plot
-    old_distances = np.expand_dims(vals[:, 0], -1).tolist()
+    old_distances = np.expand_dims(val_closest_distances[:, 0], -1).tolist()
     old_distances = [i + ["Old Whales"] for i in old_distances]
-    new_distances = [i + ["New Whales"] for i in new_vals.numpy().tolist()]
+    new_distances = [i + ["New Whales"] for i in new_whales_closest_distances.numpy().tolist()]
     df = pd.DataFrame(new_distances + old_distances, columns=['Distance', 'Kind'])
 
-    mean_old = round(np.mean(vals[:, 0]))
-    mean_new = round(np.mean(new_vals[:, 0]))
+    mean_old = np.round(np.mean(val_closest_distances[:, 0]),2)
+    mean_new = np.round(np.mean(old_distances[:, 0]),2)
     p = (p9.ggplot(data=df,
                    mapping=p9.aes(x='Distance', color='Kind'))
          + p9.geom_density()
@@ -140,7 +140,11 @@ def create_triplet_eval(model, train_ds, val_ds, train_df, val_df, new_whales_ds
     ax.barh(names, spec_acc, color='#1f77b4', alpha=0.5)
     ax.barh(names, top5_acc, color='#ff7f0e', alpha=0.7)
     ax.barh(names, acc, color='#d62728', alpha=0.8)
-    plt.title("Accuracy by class, Red=Indivum_MAP, Orange=K5_Individum_MAP, Blue=Species_MAP")
+    legend_elements = [Patch(facecolor='#1f77b4', label='Species-MAP'),
+                       Patch(facecolor='#ff7f0e', label='k5-MAP'),
+                       Patch(facecolor='#d62728', label='Individual-MAP')]
+    ax.legend(handles=legend_elements, loc='center left', bbox_to_anchor=(1, 0.5), title="Kind of Accuracy")
+    plt.title("Accuracy by Class", fontsize=18)
     plt.xticks(rotation=0)
     plt.tick_params(axis='y', labelsize=13)
     plt.tick_params(axis='x', labelsize=13)
