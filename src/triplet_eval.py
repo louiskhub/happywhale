6a3d737703e47575f7db348786098cc63b7d8dca
MockaWolke <fhammer@uni-osnabrueck.de>
2022-04-03 11:51:40 +0200

    nur noch docstrings und comments fehlen

--- src/triplet_eval.py
+++ src/triplet_eval.py
@@ -6,15 +6,17 @@ import seaborn as sns
 from visualizer import nicer_classes
 import plotnine as p9
 import pandas as pd
+import time
 from scipy.spatial.distance import cdist
 import umap
 
 
-def compute_closest_k_neighbors(a,b,k=3):
-  pairwise_dis = cdist(a,b)
-  vals , indeces = tf.nn.top_k(tf.math.negative(pairwise_dis),k)
-  vals = tf.math.negative(vals)
-  return vals,indeces
+def compute_closest_k_neighbors(a, b, k=3):
+    pairwise_dis = cdist(a, b)
+    vals, indeces = tf.nn.top_k(tf.math.negative(pairwise_dis), k)
+    vals = tf.math.negative(vals)
+    return vals, indeces
+
 
 def k_accuracy(train_df, val_df, k, closest_k_indices, indivums=True, return_mean=True):
     assert closest_k_indices.shape[1] >= k
@@ -38,68 +40,92 @@ def k_accuracy(train_df, val_df, k, closest_k_indices, indivums=True, return_mea
     else:
         return hits
 
-def calculate_class_accuracy(species,val_df):
-    df = val_df.loc[val_df["species"]==species,["correctly_labeled","species_correctly_labeled","correctly_labeled_k5"]]
+
+def calculate_class_accuracy(species, val_df):
+    df = val_df.loc[
+        val_df["species"] == species, ["correctly_labeled", "species_correctly_labeled", "correctly_labeled_k5"]]
     acc = np.mean(df["correctly_labeled"])
-    spec_acc =  np.mean(df["species_correctly_labeled"])
+    spec_acc = np.mean(df["species_correctly_labeled"])
     k5_acc = np.mean(df["correctly_labeled_k5"])
-    return np.array([acc, spec_acc,k5_acc])
+    return np.array([acc, spec_acc, k5_acc])
 
 
-def create_triplet_eval(model,train_ds,val_ds,train_df,val_df,new_whales_ds,new_whales_df,path_to_save):
+def create_triplet_eval(model, train_ds, val_ds, train_df, val_df, new_whales_ds, new_whales_df, path_to_save,name):
+    # calculate embeddings
 
-    val_preds = model.predict(val_ds, verbose=1)
-    train_preds = model.predict(train_ds, verbose=1)
-    new_whales_preds = model.predict(new_whales_ds, verbose=1)
+    print("Calculate Embeddings")
+    start_time = time.time()
 
-    vals, closest_k_indices = compute_closest_k_neighbors(val_preds, train_preds, 5)
+    val_embeddings = model.predict(val_ds, verbose=1)
+    train_embeddings = model.predict(train_ds, verbose=1)
+    new_whales_embeddings = model.predict(new_whales_ds, verbose=1)
 
-    val_df["correctly_labeled"] = k_accuracy(train_df, val_df, 1, closest_k_indices, indivums=True, return_mean=False)
-    val_df["species_correctly_labeled"] = k_accuracy(train_df, val_df, 1, closest_k_indices, indivums=False,
+    # compute closest neighbors
+
+    print(f"Finished, took {start_time - time.time()}s. Now compute closest neighbors")
+    start_time = time.time()
+
+    val_closest_distances, val_closest_k_indices = compute_closest_k_neighbors(val_embeddings, train_embeddings, 5)
+    new_whales_closest_distances, _ = compute_closest_k_neighbors(new_whales_embeddings, train_embeddings, 1)
+
+    # compute embeddings
+
+    print(f"Finished, took {start_time - time.time()}s. Now compute umap embeddings")
+    start_time = time.time()
+    reducer = umap.UMAP()
+    reduced_train_embeddings = reducer.fit_transform(train_embeddings)
+    reduced_val_embeddings = reducer.fit_transform(val_embeddings)
+
+    print(f"Finished, took {start_time - time.time()}s. Now create plots")
+
+    # compute accuracies
+    val_df["correctly_labeled"] = k_accuracy(train_df, val_df, 1, val_closest_k_indices, indivums=True,
+                                             return_mean=False)
+    val_df["species_correctly_labeled"] = k_accuracy(train_df, val_df, 1, val_closest_k_indices, indivums=False,
                                                      return_mean=False)
-    val_df["correctly_labeled_k5"] = k_accuracy(train_df, val_df, 5, closest_k_indices, indivums=True,
+    val_df["correctly_labeled_k5"] = k_accuracy(train_df, val_df, 5, val_closest_k_indices, indivums=True,
                                                 return_mean=False)
 
-    f = lambda x: np.round(x, 2)
-    over_all_acc = f(np.mean(val_df["correctly_labeled"]))
-    over_all_acc_species = f(np.mean(val_df["species_correctly_labeled"]))
-    over_all_top5_acc = f(np.mean(val_df["correctly_labeled_k5"]))
+    round = lambda x: np.round(x, 2)
 
-    sns.set_theme()
-    transform_embeddings_2d = TSNE(n_components=2, learning_rate='auto', init='random').fit_transform(train_preds)
+    over_all_acc = round(np.mean(val_df["correctly_labeled"]))
+    over_all_acc_species = round(np.mean(val_df["species_correctly_labeled"]))
+    over_all_top5_acc = round(np.mean(val_df["correctly_labeled_k5"]))
+
+    fig_info = name + f"Accuracies:  Individual-MAP: {over_all_acc}, Species-MAP: {over_all_acc_species}, k5-MAP - {over_all_top5_acc}"
+
     fig, ax = plt.subplots(figsize=(14, 14))
 
-
     for species in train_df["species"].unique():
         mask = (train_df["species"] == species).values
-        ax.scatter(transform_embeddings_2d[mask, 0], transform_embeddings_2d[mask, 1], label=nicer_classes(species))
+        ax.scatter(reduced_train_embeddings[mask, 0], reduced_train_embeddings[mask, 1], label=nicer_classes(species))
+
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), title="Species")
-    plt.title(
-        f"2d Visualisation of Embedding Space - Individum MAP: {over_all_acc}, Species MAP: {over_all_acc_species}, Top 5 MAP - {over_all_top5_acc}")
-    plt.savefig(path_to_save + "2dTSNE.png",bbox_inches='tight')
+    plt.figtext(0.5, 0.08, fig_info, wrap=True, horizontalalignment='center', fontsize=14)
+    plt.title("2d UMAP Visualisation of Train Embeddings")
 
-    transform_embeddings_3d = TSNE(n_components=3, learning_rate='auto', init='random').fit_transform(train_preds)
-    fig = plt.figure(figsize=(14, 14))
-    ax = fig.add_subplot(projection='3d')
 
-    for species in train_df["species"].unique():
-        mask = (train_df["species"] == species).values
-        ax.scatter(transform_embeddings_3d[mask, 0], transform_embeddings_3d[mask, 1], transform_embeddings_3d[mask, 2],
-                   label=nicer_classes(species))
+    plt.savefig(path_to_save + "2dUMAP_train.png", bbox_inches='tight')
+
+    fig, ax = plt.subplots(figsize=(14, 14))
+
+    for species in val_df["species"].unique():
+        mask = (val_df["species"] == species).values
+        ax.scatter(reduced_val_embeddings[mask, 0], reduced_val_embeddings[mask, 1], label=nicer_classes(species))
+
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), title="Species")
-    plt.title(
-        f"3d Visualisation of Embedding Space - Individum MAP: {over_all_acc}, Species MAP: {over_all_acc_species}, Top 5 MAP - {over_all_top5_acc}")
-    plt.savefig(path_to_save + "3dTSNE.png",bbox_inches='tight')
+    plt.figtext(0.5, 0.08, fig_info, wrap=True, horizontalalignment='center', fontsize=14)
+    plt.title("2d UMAP Visualisation of Val Embeddings")
+    plt.savefig(path_to_save + "2dUMAP_val.png", bbox_inches='tight')
 
-    new_vals, _ = compute_closest_k_neighbors(new_whales_preds, train_preds, 1)
-
+    # create daframe for density plot
     old_distances = np.expand_dims(vals[:, 0], -1).tolist()
     old_distances = [i + ["Old Whales"] for i in old_distances]
     new_distances = [i + ["New Whales"] for i in new_vals.numpy().tolist()]
     df = pd.DataFrame(new_distances + old_distances, columns=['Distance', 'Kind'])
 
-    mean_old = f(np.mean(vals[:, 0]))
-    mean_new = f(np.mean(new_vals[:, 0]))
+    mean_old = round(np.mean(vals[:, 0]))
+    mean_new = round(np.mean(new_vals[:, 0]))
     p = (p9.ggplot(data=df,
                    mapping=p9.aes(x='Distance', color='Kind'))
          + p9.geom_density()
@@ -122,8 +148,7 @@ def create_triplet_eval(model,train_ds,val_ds,train_df,val_df,new_whales_ds,new_
     plt.tick_params(axis='x', labelsize=13)
     plt.ylabel('Whale Species', fontsize=16)
     plt.xlabel('Counts', fontsize=16)
-
-    plt.savefig(path_to_save + "accuracy_by_class.png",bbox_inches='tight')
+    plt.figtext(0.5, 0.08, fig_info, wrap=True, horizontalalignment='center', fontsize=14)
+    plt.savefig(path_to_save + "accuracy_by_class.png", bbox_inches='tight')
 
     val_df.to_csv(path_to_save + "val_df.csv")
-
